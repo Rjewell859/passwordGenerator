525b9061eb87d48826529bf3f7871e69531438fe
Headwall Forest <headwallforest27@gmail.com>
2022-07-25 16:14:04 -0400

    Completed commenting, added to the readme.

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,34 @@
-// Assignment code here
+function generatePassword() {
 
-
-function generatePassword(){
-  
   // Arrays to store numbers, lower and upper case letters, and special characters.
+
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  
   var special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", "'", ",", "<", ".", "?", "/"];
-  
+
+  // Array to hold all chosen characters.
+
   var passwordArray = [];
+
+  // String to contain the password as it is built.
 
   var buildPassword = "";
 
+  // Prompt to set user specified password length. 
+
   var length = window.prompt("Enter a number between 8-128 for password length:");
-  
+
+  // Ensures user cannot continue if a number outside of the range is entered.
+
   if (length > 128) {
     window.alert("Password must be under 129 characters long.");
-  }
-  else if (length < 8) {
+  } else if (length < 8) {
     window.alert("Password must be over 8 characters long.");
-  }
-  else {
-    window.alert(special[24]);
+  } else {
+
+    // Series of if statements to select users choice of characters and store it in the passsword array.
+
     if (window.confirm("Would you like numbers?")) {
       passwordArray = passwordArray.concat(numbers);
       window.alert(passwordArray);
@@ -42,25 +47,15 @@ function generatePassword(){
       generatePassword();
     }
 
+    // This for loop creates the password, it will continue to iterate until reaching the specified password length. Each time it goes around it finds a random number
+    // within the range of the password array, rounds it down, and uses that number as an index
+    // in the array to then concatenate the character to buildPassword.
+
     for (var i = 0; i < length; i++) {
       var index = Math.floor(Math.random() * (passwordArray.length - 1))
       buildPassword = buildPassword.concat(passwordArray[index]);
-
-
-
     }
-    console.log(buildPassword);
     return buildPassword;
-
-
-
-
-
-
-
-
-
-
   }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # passwordGenerator
 This project will generate customized passwords with user defined criteria for characters and length.
+The user simply clicks the generate password button to begin the process.
+First the user is prompted for a password length in between 8 and 128 characters. 
+Then there are options for numbers, lower-case letters, upper-case letters, and special characters. 
+After all five prompts the users new random password will be displayed on the screen.
+
+
+This utilizes an array to store the selected characters and then until the specified length
+is reached, continues to concatenate random characters from that array. The key to this 
+functionality is using Math.floor in combination with Math.random multipled by the 
+characters arrays length minus one. 
+
+Link to deployed website: https://rjewell859.github.io/myPortfolio/
+
+Link to repository: https://github.com/Rjewell859/myPortfolio
+
+Screenshot: 
